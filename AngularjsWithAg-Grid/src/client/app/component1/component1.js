94a97d461318cbfc3856ba72edec639cc1dcde33
CHENCHIH HO <chenchihho@CHENCHIHdeiMac.lan>
2015-11-07 21:51:42 +0800

    加入新的ag-grid測試及更新文件

--- AngularjsWithAg-Grid/src/client/app/component1/component1.js
+++ AngularjsWithAg-Grid/src/client/app/component1/component1.js
@@ -7,33 +7,77 @@
         .module('app')
         .controller('Component1', Component1);
 
-    Component1.$inject = ['toastr']; 
+    Component1.$inject = ['toastr', 'fakeData']; 
 
-    function Component1(toastr) {
+    function Component1(toastr, fakeData) {
         /* jshint validthis:true */
         var vm = this;
         vm.title = 'Component1';
         vm.appendRow = appendRow;
         vm.updateRowByModel = updateRowByModel;
+        vm.updateExistingRow = updateExistingRow;
+        vm.appendDataIndex = 50;
 
         //-----------------------
         var columnDefs = [
-            {headerName: "Make", field: "make", suppressSorting: true, suppressMenu: true},
-            {headerName: "Model", field: "model", suppressSorting: true, suppressMenu: true},
-            {headerName: "Price", field: "price"}
+            {headerName: "Id", field: "Id"},            
+            {headerName: "Name", field: "Name", suppressSorting: true, suppressMenu: true
+                ,cellClass: function(params) {
+                    console.log(vm.appendDataIndex);
+                    return 'flash';
+                    // if (params.value==='xxx') {
+                    //     return 'flash';
+                    // } else {
+                    //     return null;
+                    // }
+                }
+                // ,cellRenderer: function(params) {
+                //     console.log(params);
+                //     // if (params.value.startsWith('A'))
+                //     if (params.value==='xxx') {
+                //         return params.value.toUpperCase();
+                //         // return '<span style="color:red;">▲</span>' + params.value.toUpperCase();
+                //     } else {
+                //         return params.value.toLowerCase();
+                //     }
+                // }
+            }
+            ,{headerName: "Tel", field: "Tel", suppressSorting: true, suppressMenu: true},
+            {headerName: "Country", field: "Country",             
+            cellClassRules: {
+                    'error': function(params) { return params.value === 'Grenada'},
+                    'info': function(params) { return params.value === 'Zambia'}
+                }
+            },
+            {headerName: "Email", field: "Email", cellClass: function(params) { return ['info','error-block']; }},
+            {headerName: "CountryName", valueGetter: countryNameValueGetter}
         ];
-    
-        var rowData = [
-            {make: "Toyota", model: "Celica", price: 35000},
-            {make: "Ford", model: "Mondeo", price: 32000},
-            {make: "Porsche", model: "Boxter", price: 72000}
-        ];
-        
-        var basicGridOption = {
+
+        function countryNameValueGetter(params) {
+                return params.data.Country + ',' + params.data.Name;
+            }
+
+
+        vm.gridOptions = {
             columnDefs: columnDefs,
-            rowData: rowData,
+            rowData: [],
             enableSorting: true,
-            enableFilter: true
+            enableFilter: true,
+            onReady: onGridReady,
+            columnValueChanged: onCellValueChanged
+            // ,getRowClass: function(params) {
+            //     console.log(params);
+            //     return 'flash';
+            // }
+            // ,getRowClass: function(params) {
+            //                 //console.log(params);
+            //                 if (params.data.isNew) {
+            //                     delete params.data.isNew;
+            //                     return 'flash';
+            //                 } else {
+            //                     return '';
+            //                 }
+            //             }
         }
         //-----------------------
         activate();
@@ -45,18 +89,38 @@
          
         //-----------------------
         
+        function onGridReady() {
+            console.log('onGridReady');
+        }
+        
+        function onCellValueChanged(params) {
+            console.log(params);
+        }
+        
         function createBasicGrid() {
-            vm.gridOptions = basicGridOption;
+            vm.gridOptions.rowData = fakeData.getContacts();
         }
         
         function appendRow() {
-            vm.gridOptions.rowData.push({make: "Audi", model: "Q5", price: 32000});
-            vm.gridOptions.api.onNewRows();
+            var newData = fakeData.getContact(vm.appendDataIndex++);
+            newData.isNew = true;
+            vm.gridOptions.rowData.splice(0,0, newData);
+            //onNewRow()已不再使用
+            //vm.gridOptions.api.onNewRows();
+            vm.gridOptions.api.setRowData(vm.gridOptions.rowData);
         }
         
-        function renewData() {
-            var nodes = [{make: "Audi", model: "Q5", price: 32000}];
-            vm.gridOptions.api.setRowData(nodes);
+        function updateExistingRow() {
+            var updatedNodes = [];
+            vm.gridOptions.api.forEachNode( function(node) {
+                if (node.data.Id === 1 || node.data.Id === 48) {
+                    // node.data.name = makeid();
+                    node.data.Name = 'xxx';
+                    node.data.isNew = true;
+                    updatedNodes.push(node);
+                }
+            });
+            vm.gridOptions.api.refreshRows(updatedNodes);
         }
         
         //找出grid內部的virtual structure中的資料並比對做更新
@@ -76,6 +140,22 @@
             // now tell the grid it needs refresh all these rows
             vm.gridOptions.api.refreshRows(updatedNodes);
         };
+        
+        function makeFakeContactData() {
+
+        }
+        
+        function makeid()
+        {
+            var text = "";
+            var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        
+            for( var i=0; i < 5; i++ )
+                text += possible.charAt(Math.floor(Math.random() * possible.length));
+        
+            return text;
+        }
+        
     }
 
 })();
